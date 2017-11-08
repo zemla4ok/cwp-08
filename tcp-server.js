8b9fc10d2f2b307372a55eab4af290bb058feb5e
zemla4ok <dima-vital@live.com>
2017-11-08 14:39:44 +0300

    switching method of request

--- tcp-server.js
+++ tcp-server.js
@@ -9,24 +9,29 @@ let workers = [];
 const server = net.createServer((client) => {
     client.setEncoding('utf8');
 
-    client.on('data', (data) => {
-
+    client.on('data', (data, err) => {
+        let req = JSON.parse(data);
+        if (req.key === 'worker'){
+            switch(req.method){
+                case 'start':
+                {
+                    let id = getUniqID();
+                    let file = `./files/${id}.json`;
+                    let worker = childProcess.spawn('node', ['worker.js', file, `${interval}`]);
+                    let date = new Date();
+                    worker.id = id;
+                    worker.startedOn = date.toISOString();
+                    worker.file = file;
+                    workers.push(worker);
+                }
+            }
+        }
     });
 
     client.on('end', () => {
         console.log(`Client ${client.id} disconnected`);
     });
 });
-
-function startWorker(interval) {
-    let id = getUniqID();
-    let file = `./files/${Date.now() + seed++}.json`;
-    let worker = childProcess.spawn('node', ['worker.js', file, `${interval}`]);
-    let date = new Date();
-    worker.startedOn = date.toISOString();
-    worker.file = file;
-    worker.push(worker);
-}
 
 server.listen(port, () => {
     console.log(`Server listening on localhost:${port}`);
